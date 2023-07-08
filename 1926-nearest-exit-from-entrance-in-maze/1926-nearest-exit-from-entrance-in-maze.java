4210a2ce6958922c8af77431f7e01caff8950350
Mohana Sundaram K <125396976+MohanaSundaram06@users.noreply.github.com>
2023-07-08 18:43:18 +0530

    Time: 7 ms (81.73%), Space: 44.3 MB (80.30%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -3,43 +3,50 @@ class Solution {
         
         Queue<int[]> q = new LinkedList<>();
         
+        q.add(new int[]{entrance[0],entrance[1]});
+        
+        int count = 0;
+        
         int r = maze.length;
-        int c = maze[0].length;
+        int c= maze[0].length;
         
-        q.add(new int[]{entrance[0], entrance[1]});
-        maze[entrance[0]][entrance[1]] = '+';
-
-        int ans = 0;
+        boolean[][] visited = new boolean[r][c];
+        visited[entrance[0]][entrance[1]] = true;
         
-        int[][] dir = new int[][] {{0,1} , {0,-1}, {1,0}, {-1,0}};
+        int[][] dir = new int[][] {{0,1}, {0, -1}, { 1, 0}, {-1, 0}};
         
-        while(q.size() > 0){
-            int size = q.size();
-            ans ++;
+        while( q.size() > 0){
+            int size= q.size();
+            count++;
             while( size > 0){
                 
-                int[] p = q.remove();
-                size --;
-                         
+                int[] pos = q.remove();
+                
+                size--;
+                
                 for(int i = 0; i < 4; i++){
-                    int x = p[0] + dir[i][0];
-                    int y = p[1] + dir[i][1];
-
-                    if( x < 0 || y < 0 || x >= r || y >= c || maze[x][y] == '+') continue;
+                    int x = pos[0] + dir[i][0];
+                    int y = pos[1] + dir[i][1];
                     
-                    if(x == 0 || y == 0 || x == r - 1 || y == c - 1) return ans;
+                    if( x < 0 || y < 0 || x >= r || y >= c || visited[x][y] || maze[x][y] == '+') continue;
+                    
+                    if(x== 0 || y == 0 || x == r - 1 || y == c - 1) return count;
                     q.add(new int[]{x, y});
-                    maze[x][y] = '+';
-
+                    visited[x][y] = true;
                 }
             }
-             
         }
         
         return -1;
-        
     }
 }
+
+
+
+
+
+
+
 
 
 
