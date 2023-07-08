7f35941e0e47946dc3462e66e8a6e4357b0ebe2e
Mohana Sundaram K <125396976+MohanaSundaram06@users.noreply.github.com>
2023-07-08 19:10:41 +0530

    Time: 94 ms (96.65%), Space: 119.1 MB (39.63%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,9 +1,9 @@
 class Solution {
-    int time = 0;
+    public int time;
     
-    public void constructGraph(int n, List<List<Integer>> connections, ArrayList<ArrayList<Integer>> graph ){
+    public void constructGraph( ArrayList<ArrayList<Integer>> graph,int n, List<List<Integer>> connections ){
         
-        for(int i =0 ; i < n; i++) graph.add(new ArrayList<>());
+        for(int i = 0; i < n; i++) graph.add(new ArrayList<>());
         
         for(List<Integer> edge : connections){
             int u = edge.get(0);
@@ -11,44 +11,45 @@ class Solution {
             
             graph.get(u).add(v);
             graph.get(v).add(u);
+            
         }
     }
-        
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
         
+        int[] dist = new int[n];
+        int[] lowDist = new int[n];
+        
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        constructGraph(n, connections, graph);
+        
+        constructGraph(graph, n, connections);
+        
+        boolean[] visited = new boolean[n];
         
         List<List<Integer>> bridge = new ArrayList<>();
         
-        boolean[] visited = new boolean[n];
-        int[] dist = new int[n];
-        int[] low = new int[n];
-        
-        dfs(0, -1, bridge, dist, low, visited, graph);
+        dfs( 0,  -1, dist, lowDist, graph, bridge, visited);
         
         return bridge;
-        
     }
     
-    public void dfs(int src, int parent, List<List<Integer>> bridge, int[] dist, int[] low,
-                    boolean[] visited, ArrayList<ArrayList<Integer>> graph ){
+    public void dfs(int src, int parent , int[]  dist, int[] low ,  
+                    ArrayList<ArrayList<Integer>> graph, List<List<Integer>> bridge, boolean[] visited){
         
+        dist[src] = low[src] = time;
+        time++;
         visited[src] = true;
-        low[src] = dist[src] = time;
-        time ++;
         
         for(int nbr : graph.get(src)){
             
-            if(nbr == parent) continue;
+            if( nbr == parent) continue;
             
-            if(!visited[nbr]){
-                
-                dfs(nbr, src, bridge, dist, low, visited, graph);
+            if(!visited[nbr]){             
+                dfs(nbr, src, dist, low, graph, bridge, visited);
                 
                 low[src] = Math.min(low[src], low[nbr]);
                 
                 if(dist[src] < low[nbr]) bridge.add(Arrays.asList(src,nbr));
+                
             }
             else{
                 low[src] = Math.min(low[src], dist[nbr]);
@@ -56,6 +57,9 @@ class Solution {
         }
     }
 }
+
+
+
 
 
 
